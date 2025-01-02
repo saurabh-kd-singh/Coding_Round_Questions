90c7bf0598108777d250cfbad16c285baab6d383
saurabh-kd-singh <sksingh5087@gmail.com>
2025-01-02 18:14:39 +0530

    Longest Substring Without Repeating Character

---
+++ Sliding_Window/LongestSubstringWithoutRepeatingCharacter.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int lengthOfLongestSubstring(string s)
+{
+    int n = s.length();
+    int maxLength = 0;
+    vector<int> charIndex(128, -1);
+    int left = 0;
+
+    for (int right = 0; right < n; right++)
+    {
+        if (charIndex[s[right]] >= left)
+        {
+            left = charIndex[s[right]] + 1;
+        }
+        charIndex[s[right]] = right;
+        maxLength = max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+}
+
+int main()
+{
+    string s = "abcabcbb";
+    cout << lengthOfLongestSubstring(s);
+    return 0;
+}
