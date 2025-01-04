44068f1e771d6cfd1d52c0d3f00c7ed922838877
saurabh-kd-singh <sksingh5087@gmail.com>
2025-01-05 00:02:55 +0530

    Valid Paranthesis

---
+++ Strings/ValidParanthesis.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> st;
+    for (char c : s)
+    {
+        if (c == '(' || c == '{' || c == '[')
+        {
+            st.push(c);
+        }
+        else
+        {
+            if (st.empty() || (c == ')' && st.top() != '(') || (c == '}' && st.top() != '{') || (c == ']' && st.top() != '['))
+            {
+                return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
+}
+
+int main()
+{
+    string s = " (({{[[]]}}))";
+    cout << isValid(s);
+    return 0;
+}
